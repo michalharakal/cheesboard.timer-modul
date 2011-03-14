adb55148378bf5f290c8516c262a2aec4cac9e41
Michal Harakal <michal.harakal@googlemail.com>
2011-03-15 00:25:44 +0100

    rolade as arduino library for testing with arduino as master

---
+++ libraries/rolade/rolade.h
@@ -0,0 +1,29 @@
+/*
+  rolade.h
+*/
+
+#ifndef ROLADEDATA_HPP
+#define ROLADEDATA_HPP
+
+#include <stdio.h>
+#include <inttypes.h>
+
+
+extern "C" {
+#include "rolade_struc.h"
+};
+
+
+
+class RoladeProperties:private RoladePropertiesData
+{
+  public:
+    RoladeProperties();
+    ~RoladeProperties();
+	void SetAutomaticInterval(uint8_t h_start, uint8_t m_start, uint8_t h_end, uint8_t m_end);
+	void Serialize(void *dest, size_t *size);
+     void Deserialize(void *src);
+  private:
+};
+
+#endif
